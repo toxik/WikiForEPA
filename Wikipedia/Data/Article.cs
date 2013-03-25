2a7a9790818ddd13e164e4da289a2f99070b8055
andraciorici <andra.ciorici@deskperience.com>
2013-03-25 23:50:17 +0200

    build fix

---
+++ Wikipedia/Data/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wikipedia.Data
+{
+    public partial class Article
+    {
+        public string Name
+        {
+            get
+            { return Versions.OrderByDescending(ver => ver.CreateDate).First().Name; }
+        }
+        public string Content
+        {
+            get
+            { return Versions.OrderByDescending(ver => ver.CreateDate).First().Content; }
+        }
+    }
+}
---
+++ Wikipedia/Data/Domain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wikipedia.Data
+{
+    public partial class Domain
+    {
+        public dynamic LatestArticles
+        {
+            get
+            { return Articles.OrderByDescending(art => art.CreateDate).Take(5); }
+        }
+    }
+}
