8f836e193785c9fe421f591821dff14b9d4f7465
dorinelromeo <dorinel.romeo@gmail.com>
2013-03-26 21:00:35 +0200

    Add view article

--- Wikipedia/Article.aspx
+++ Wikipedia/Article.aspx
@@ -3,3 +3,22 @@
 
 <asp:Content ID="HeaderContent" ContentPlaceHolderID="HeadContent" runat="server">
 </asp:Content>
+
+<asp:Content ID="ActionsContent" ContentPlaceHolderID="ActionsBar" runat="server">
+    <span runat="server" id="EditAction" visible="false">
+        <a runat="server" id="EditLink" href="">Edit</a>
+    </span>
+    <span runat="server" id="ProtectAction" visible="false">
+        <span class="separator">|</span>
+        <asp:LinkButton runat="server" ID="ProtectLink" OnClick="ProtectToggle">Protect</asp:LinkButton>
+    </span>
+    <span runat="server" id="VersionAction" visible="false">
+        <span class="separator">|</span>
+        <a runat="server" id="VersionsLinks" href="">Revision history</a>
+    </span>
+</asp:Content>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1><asp:Label runat="server" ID="ArticleName" /></h1>
+    <asp:Label runat="server" ID="ArticleContent" />
+</asp:Content>
--- Wikipedia/Article.aspx.cs
+++ Wikipedia/Article.aspx.cs
@@ -11,8 +11,68 @@ namespace Wikipedia
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-          
+            using (var eds = new Data.WikipediaEntities())
+            {
+                string stringid = Request.QueryString["id"];
+                int id;
+
+                if (int.TryParse(stringid, out id))
+                {
+                    var article = eds.Articles.Where(art => art.Id == id).First();
+
+                    ArticleName.Text = article.Name;
+                    ArticleContent.Text = Textile.TextileFormatter.FormatString(article.Content);
+
+                    if (!article.IsProtected || Request.IsAuthenticated)
+                    {
+                        EditAction.Visible = true;
+                        EditLink.HRef = "~/ArticleEdit.aspx?id=" + article.Id;
+                    }
+                    if (article.UserName == User.Identity.Name || User.IsInRole("Editor"))
+                    {
+                        ProtectAction.Visible = true;
+                        if (article.IsProtected) { ProtectLink.Text = "UnProtect"; }
+                    }
+                    if (User.IsInRole("Editor"))
+                    {
+                        VersionAction.Visible = true;
+                        VersionsLinks.HRef = "~/ArticleVersions.aspx?id=" + article.Id;
+                    }
+                }
+                else
+                { Response.Redirect("~"); }
+            }
         }
 
+        protected void ProtectToggle(object sender, EventArgs e)
+        {
+            using (var eds = new Data.WikipediaEntities())
+            {
+                string stringid = Request.QueryString["id"];
+                int id;
+
+                if (int.TryParse(stringid, out id))
+                {
+                    var article = eds.Articles.Where(art => art.Id == id).First();
+
+                    if (article.UserName == User.Identity.Name || User.IsInRole("Editor"))
+                    {
+                        if (article.IsProtected)
+                        {
+                            article.IsProtected = false;
+                            ProtectLink.Text = "Protect";
+                        }
+                        else
+                        {
+                            article.IsProtected = true;
+                            ProtectLink.Text = "UnProtect";
+                        }
+                    }
+
+                    eds.SaveChanges();
+                }
+            }
+
+        }
     }
 }
