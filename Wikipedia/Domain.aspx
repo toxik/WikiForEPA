554c60cee06f4fe477485a80f99b232ac1de0d1a
andraciorici <andra.ciorici@deskperience.com>
2013-04-11 23:14:58 +0200

    See articles by domain

---
+++ Wikipedia/Domain.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Layouts/Site.Master" AutoEventWireup="true"
+    CodeBehind="Domain.aspx.cs" Inherits="Wikipedia.Domain" %>
+
+<asp:Content ID="HeaderContent" ContentPlaceHolderID="HeadContent" runat="server">
+    <link href="Styles/GridView.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+
+<asp:Content ID="ActionsContent" ContentPlaceHolderID="ActionsBar" runat="server">
+    <span runat="server" id="NewAction" visible="false">
+        <a runat="server" id="NewLink" href="">New</a>
+    </span>
+</asp:Content>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1 runat="server" id="DomainName" visible="false"></h1>
+    <asp:GridView runat="server" ID="ArticlesGridView" CellPadding="4"
+        GridLines="Vertical" ForeColor="Black"
+        AutoGenerateColumns="False" AllowSorting="True" OnSorting="GV_Sorting"
+        BackColor="White" BorderColor="#DEDFDE" BorderStyle="None"
+        BorderWidth="1px">
+        <AlternatingRowStyle BackColor="LightGray" />
+        <Columns>
+            <asp:TemplateField HeaderText="Article Name" SortExpression="Name">
+                <ItemTemplate>
+                    <a runat="server" href='<%# "~/Article.aspx?id=" + Eval("Id") %>'><%# Eval ("Name") %></a>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="UserName" HeaderText="Username"
+                SortExpression="UserName" />
+            <asp:CheckBoxField DataField="IsProtected" HeaderText="Protected"
+                SortExpression="IsProtected" />
+            <asp:BoundField DataField="CreateDate" HeaderText="Created"
+                SortExpression="CreateDate" />
+        </Columns>
+        <EmptyDataTemplate>No Articles Found.</EmptyDataTemplate>
+        <FooterStyle BackColor="#CCCC99" />
+        <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+        <RowStyle BackColor="#F7F7DE" />
+        <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#FBFBF2" />
+        <SortedAscendingHeaderStyle BackColor="#848384" />
+        <SortedDescendingCellStyle BackColor="#EAEAD3" />
+        <SortedDescendingHeaderStyle BackColor="#575357" />
+    </asp:GridView>
+</asp:Content>
---
+++ Wikipedia/Domain.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace Wikipedia
+{
+    public partial class Domain : System.Web.UI.Page
+    {
+        /* Properties */
+        private SortDirection GV_SortDirection
+        {
+            get
+            {
+                if (ViewState["sort_direction"] == null)
+                    ViewState["sort_direction"] = SortDirection.Ascending;
+                return (SortDirection) ViewState["sort_direction"];
+            }
+            set
+            { ViewState["sort_direction"] = value; }
+        }
+
+        /* Functions */
+        private void GV_Sort(string sort_expression)
+        {
+            var articles = ViewState["articles"] as DataTable;
+
+            DataView view = new DataView(articles);
+            view.Sort = sort_expression;
+            ArticlesGridView.DataSource = view;
+            ArticlesGridView.DataBind();
+        }
+
+        /* Events */
+        protected void GV_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            if (GV_SortDirection == SortDirection.Ascending)
+            {
+                GV_SortDirection = SortDirection.Descending;
+                GV_Sort (e.SortExpression + " DESC");
+            }
+            else
+            {
+                GV_SortDirection = SortDirection.Ascending;
+                GV_Sort (e.SortExpression + " ASC");
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (var eds = new Data.WikipediaEntities())
+                {
+                    string stringid = Request.QueryString["id"];
+                    int id;
+
+                    if (int.TryParse(stringid, out id))
+                    {
+                        NewAction.Visible = true;
+                        NewLink.HRef = "ArticleNew.aspx?id=" + id;
+
+                        var domain = eds.Domains.Where(dom => dom.Id == id).FirstOrDefault();
+                        if (domain != null)
+                        {
+                            Page.Title = "Wikipedia:" + domain.Name;
+                            DomainName.InnerText = domain.Name;
+                            DomainName.Visible = true;
+                        }
+
+                        var articles = eds.Articles.Where(art => art.DomainId == id);
+
+                        DataTable table = new DataTable();
+                        table.Columns.Add("Id");
+                        table.Columns.Add("Name");
+                        table.Columns.Add("UserName");
+                        table.Columns.Add("IsProtected");
+                        table.Columns.Add("CreateDate");
+
+                        table.Columns["Id"].DataType = System.Type.GetType("System.Int32");
+                        table.Columns["Name"].DataType = System.Type.GetType("System.String");
+                        table.Columns["UserName"].DataType = System.Type.GetType("System.String");
+                        table.Columns["IsProtected"].DataType = System.Type.GetType("System.Boolean");
+                        table.Columns["CreateDate"].DataType = System.Type.GetType("System.DateTime");
+
+                        foreach (var art in articles)
+                        {
+                            DataRow row = table.NewRow();
+                            row["Id"] = art.Id;
+                            row["Name"] = art.Name;
+                            row["UserName"] = art.UserName;
+                            row["IsProtected"] = art.IsProtected;
+                            row["CreateDate"] = art.CreateDate;
+
+                            table.Rows.Add(row);
+                        }
+                        
+                        ViewState["articles"] = table;
+                        DataView view = new DataView(table);
+                        view.Sort = "Name ASC";
+
+                        ArticlesGridView.DataSource = view;
+                        ArticlesGridView.DataBind();
+                    }
+                }
+            }
+        }
+
+       
+    }
+}
