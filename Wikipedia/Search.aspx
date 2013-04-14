372279d0c0a805f475dd5a24388df50a8baf801b
omegadany <omega_dany@yahoo.com>
2013-04-14 19:38:15 +0200

    Add search article functionality

---
+++ Wikipedia/Search.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Wikipedia:Search" Language="C#" MasterPageFile="~/Layouts/Site.Master" AutoEventWireup="true"
+    CodeBehind="Search.aspx.cs" Inherits="Wikipedia.Search" %>
+
+<asp:Content ID="HeadContent" ContentPlaceHolderID="HeadContent" runat="server">
+    <link href="Styles/GridView.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Search results:</h1>
+    <asp:GridView runat="server" ID="ArticlesGridView" CellPadding="4" GridLines="None"
+        ForeColor="#333333" AutoGenerateColumns="False">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <Columns>
+            <asp:TemplateField HeaderText="Name" SortExpression="Name">
+                <ItemTemplate>
+                    <a runat="server" href='<%# "~/Article.aspx?id=" + Eval("Id") %>'>
+                        <%# Eval ("Name") %></a>
+                </ItemTemplate>
+            </asp:TemplateField>
+            <asp:BoundField DataField="UserName" HeaderText="User" 
+                SortExpression="UserName" />
+            <asp:BoundField DataField="Domain" HeaderText="Categories" 
+                SortExpression="Domain" />
+            <asp:CheckBoxField DataField="IsProtected" HeaderText="Editable" 
+                SortExpression="IsProtected" />
+            <asp:BoundField DataField="CreateDate" HeaderText="CreateDate" SortExpression="CreateDate" />
+        </Columns>
+        <FooterStyle BackColor="#5D7B9D" ForeColor="White" Font-Bold="True" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+    </asp:GridView>
+</asp:Content>
---
+++ Wikipedia/Search.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+
+namespace Wikipedia
+{
+    public partial class Search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                using (var eds = new Data.WikipediaEntities())
+                {
+                    var articles = eds.Articles;
+
+                    DataTable table = new DataTable();
+                    table.Columns.Add("Id");
+                    table.Columns.Add("Name");
+                    table.Columns.Add("UserName");
+                    table.Columns.Add("Domain");
+                    table.Columns.Add("IsProtected");
+                    table.Columns.Add("CreateDate");
+
+                    table.Columns["Id"].DataType = System.Type.GetType("System.Int32");
+                    table.Columns["Name"].DataType = System.Type.GetType("System.String");
+                    table.Columns["UserName"].DataType = System.Type.GetType("System.String");
+                    table.Columns["Domain"].DataType = System.Type.GetType("System.String");
+                    table.Columns["IsProtected"].DataType = System.Type.GetType("System.Boolean");
+                    table.Columns["CreateDate"].DataType = System.Type.GetType("System.DateTime");
+
+                    foreach (var art in articles)
+                    {
+                        DataRow row = table.NewRow();
+                        row["Id"] = art.Id;
+                        row["Name"] = art.Name;
+                        row["UserName"] = art.UserName;
+                        row["Domain"] = art.Domain.Name;
+                        row["IsProtected"] = art.IsProtected;
+                        row["CreateDate"] = art.CreateDate;
+
+                        table.Rows.Add(row);
+                    }
+
+                    ViewState["articles"] = table;
+                }
+            }
+
+            string searchtext = Request.QueryString["q"];
+
+            var view = new DataView (ViewState["articles"] as DataTable);
+            view.RowFilter = String.Format("Name LIKE '%{0}%'", searchtext);
+            view.Sort = "Name ASC";
+
+            ArticlesGridView.DataSource = view;
+            ArticlesGridView.DataBind();
+        }
+    }
+}
